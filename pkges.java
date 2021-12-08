27008261026c280c82618976762beddaf9866c39
ektasawant2001 <68429412+ektasawant2001@users.noreply.github.com>
2021-12-08 22:56:00 +0530

    Add files via upload

---
+++ pkges.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+import java.util.ArrayList;
+//import java.util.*;
+class pkges{
+	public static void main(String[] args){
+	/*java.util.Date date = new java.util.Date();*/ //Fully Qualified names
+	Date date ;
+	ArrayList list;
+	// java.sql.Date date2;
+	System.out.println("HIIIIIIIIIIIII");
+	}
+}
+
+
