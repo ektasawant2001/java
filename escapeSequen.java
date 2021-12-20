439720bbb65a954e0d8b1fcac177bdeafc72d0f3
ektasawant2001 <68429412+ektasawant2001@users.noreply.github.com>
2021-12-20 14:21:35 +0530

    Add files via upload

---
+++ escapeSequen.java
@@ -0,0 +1,12 @@
+public class escapeSequen{
+	public static void main(String[] args){
+		String s1="Hello \"Mr.\" Jhon, \n\tHow are you\\";
+		System.out.println(s1);
+		
+		char[] s2={'\'','"','l','l','o'};
+		System.out.println(s2);
+	
+	
+	
+	}
+}
