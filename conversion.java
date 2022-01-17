13a67e32083f15a5158dd14570aba2c63a3acc3a
ektasawant2001 <68429412+ektasawant2001@users.noreply.github.com>
2022-01-17 19:56:30 +0530

    Add files via upload

---
+++ conversion.java
@@ -0,0 +1,14 @@
+public class conversion{
+		public static void main(String[] args){
+			double a,b,c,d;
+			a=23;
+			b=23*1000;
+			System.out.println("The conversion of km to m is : "+b);
+			c=78.5;
+			d=78.5*(0.001);
+			
+			System.out.println("The conversion of m to km is : "+d);
+		}
+}
+			
+			
